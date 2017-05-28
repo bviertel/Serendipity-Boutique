5356194a6cc2daeddf87f53ed77454bb6f9cebeb
Brandon Viertel <bviertel@gmail.com>
2017-05-28 14:07:22 -0400

    Added Firebase

--- Serendipity Boutique/ViewController.swift
+++ Serendipity Boutique/ViewController.swift
@@ -14,11 +14,13 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
+	
+	
 
-	override func didReceiveMemoryWarning() {
+	/*override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
-	}
+	}*/
 
 
 }
